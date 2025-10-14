ffa181604a5573b660e945ea23c597a6e94c1776
GRIKZHANA <grikzhanadaolaguput20@gmail.com>
2025-10-14 22:47:37 +0530

    Add files via upload

---
+++ Question52.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    // 1st group - 1 star
+    for (i = 1; i <= 1; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+
+    // 2nd group - 2 stars
+    for (i = 1; i <= 2; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+
+    // 3rd group - 5 stars
+    for (i = 1; i <= 5; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+
+    // 4th group - 3 stars
+    for (i = 1; i <= 3; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+
+    // 5th group - 1 star
+    for (i = 1; i <= 1; i++) {
+        printf("*\n");
+    }
+
+    return 0;
+}
