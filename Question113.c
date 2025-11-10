c4077c5fa4a157787aab90671ce6e676d0d942cb
GRIKZHANA <grikzhanadaolaguput20@gmail.com>
2025-11-10 23:45:14 +0530

    Add files via upload

---
+++ Question113.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+// Function to sort the array using simple Bubble Sort
+void sortArray(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int n, k;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter k (to find kth smallest element): ");
+    scanf("%d", &k);
+
+    if (k < 1 || k > n) {
+        printf("Invalid value of k.\n");
+        return 0;
+    }
+
+    // Sort the array
+    sortArray(arr, n);
+
+    // Print the kth smallest element
+    printf("The %dth smallest element is: %d\n", k, arr[k - 1]);
+
+    return 0;
+}
